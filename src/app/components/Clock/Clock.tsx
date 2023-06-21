57c6a18a3ee7e1139346dd21b96062a802adb904
chilleno <antonion.gonzalezf@gmail.com>
2023-06-21 11:03:20 -0400

    Update Clock.tsx
    - add responsive to the clock

--- src/app/components/Clock/Clock.tsx
+++ src/app/components/Clock/Clock.tsx
@@ -13,16 +13,16 @@ const Clock: React.FC = () => {
     }, []);
 
     return (
-        <div className="h-auto bg-gray-900 text-white font-bold p-2">
-            <div className="flex justify-end text-xl">
+        <div className="sm:flex sm:items-center sm:justify-end lg:items-end lg:justify-end lg:flex-col h-auto bg-gray-900 text-white font-bold p-2">
+            <div className="flex justify-end lg:text-xl sm:text-xs">
                 {dateState.toLocaleDateString('en-GB', {
                     weekday: 'short',
                     day: 'numeric',
                     month: 'short',
                 })}
             </div>
-            <div className="flex justify-end text-5xl">
-                <ClockIcon className="h-12 w-12 text-white-500 mr-1" />
+            <div className="flex items-center justify-end lg:text-5xl sm:text-xs sm:ml-2">
+                <ClockIcon className="sm:hidden lg:inline lg:h-12 lg:w-12 text-white-500" />
                 {dateState.toLocaleString('en-US', {
                     hour: 'numeric',
                     minute: 'numeric',
