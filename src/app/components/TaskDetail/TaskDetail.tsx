797f13ed5912458544cb2221ca8e48955ff09ba9
chilleno <antonion.gonzalezf@gmail.com>
2023-06-24 18:48:38 -0400

    Update TaskDetail.tsx
    - add responsive content
    - delete guide bg colors
    - add a little style to the component

--- src/app/components/TaskDetail/TaskDetail.tsx
+++ src/app/components/TaskDetail/TaskDetail.tsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { ArrowLeftIcon } from '@heroicons/react/24/solid';
+import { CheckIcon } from '@heroicons/react/24/solid';
 
 interface TaskDetailProps {
     header: string;
@@ -25,24 +27,27 @@ const TaskDetail: React.FC<TaskDetailProps> = ({ header, description, nextTask, 
     }, [header, description]);
 
     return (
-        <div className="ml-5 mr-5 mt-5 h-auto bg-gray-900">
+        <div className="xl:p-10 lg:p-5 sm:p-2 h-screen bg-blue-900">
             <div className="flex">
-                <div className="w-3/5">
-                    <h1 className="bg-white text-black rounded-full px-4 py-2 text-3xl font-bold">{headerState}</h1>
+                <div className="xl:w-3/5 lg:w-3/5 sm:w-4/6">
+                    <h1 className="bg-white text-gray-900 rounded-full px-4 xl:py-2 lg:py-2 sm:py-2 xl:text-4xl lg:text-md sm:text-xs font-bold shadow-md shadow-gray-900">{headerState}</h1>
                 </div>
-                <div className="w-2/5 flex flex-col justify-center">
+                <div className="xl:w-2/5 lg:w-2/5 sm:w-2/6 flex flex-col justify-center">
                     <div className="flex justify-center">
-                        <button onClick={handleNextTask} className="px-4 py-2 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm">
-                            Complete Task
+                        <button onClick={handleNextTask} className="px-4 xl:py-4 lg:py-2 sm:py-1 xl:text-md font-semibold xl:text-sm lg:text-sm bg-cyan-500 text-white rounded-full shadow-md shadow-gray-900">
+                            <span className="sm:hidden lg:inline xl:inline">Complete Task</span>
+                            <CheckIcon className="xl:hidden lg:hidden sm:inline sm:w-3 sm:h-3 text-white-500" />
                         </button>
-                        <button onClick={handlePreviousTask} className="ml-5 px-4 py-2 font-semibold text-sm bg-gray-500 text-white rounded-full shadow-sm">
-                            {"<-"}
+                        <button onClick={handlePreviousTask} className="xl:ml-5 lg:ml-5 sm:ml-3 px-4 xl:py-4 lg:py-2 sm:py-1 font-semibold bg-gray-500 text-white rounded-full shadow-md shadow-gray-900">
+                            <ArrowLeftIcon className="xl:h-6 xl:w-6 lg:h-6 lg:w-6 sm:w-3 sm:h-3 text-white-500" />
                         </button>
                     </div>
                 </div>
             </div>
             <div className="mt-5">
-                <p className="bg-white text-black rounded-full px-4 py-2 text-xl font-bold">{descriptionState}</p>
+                <p className="bg-white text-gray-900 max-h-16 min-h-[300px] sm:min-h-[150px] rounded-3xl px-5 py-3 xl:text-2xl lg:text-sm sm:text-[12px] font-bold shadow-md shadow-gray-900 overflow-y-auto">
+                    {descriptionState}
+                </p>
             </div>
         </div>
     );
