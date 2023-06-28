301b71f8d36f216e563888b18eef4277d9a80cb2
chilleno <antonion.gonzalezf@gmail.com>
2023-06-28 18:53:13 -0400

    Update TaskList.tsx
    - fix warnings on console

--- src/app/components/TaskList/TaskList.tsx
+++ src/app/components/TaskList/TaskList.tsx
@@ -47,14 +47,14 @@ const TaskList: React.FC<TaskListProps> = ({ tasks, currentTaskIndex }) => {
                             <ArcherElement
                                 key={task.id}
                                 id={task.order.toString()}
-                                relations={[
+                                relations={(task.order + 1) < tasks.length ? [
                                     {
                                         targetId: (task.order + 1).toString(),
                                         targetAnchor: 'top',
                                         sourceAnchor: 'bottom',
                                         style: { strokeColor: 'white', strokeWidth: 5, endMarker: false, noCurves: true, },
                                     },
-                                ]}
+                                ] : []}
                             >
                                 <div
                                     key={task.id}
