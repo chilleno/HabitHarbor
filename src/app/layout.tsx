992230b1e38d12544a7db6927617ab3ee06883da
chilleno <antonion.gonzalezf@gmail.com>
2023-07-30 22:04:17 -0400

    Update layout.tsx
    - implements vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
       </body>
+      <Analytics />
     </html>
   )
 }
