8551aa8c521f9a1f8699a1a24d57c8f05691fc6a
chilleno <antonion.gonzalezf@gmail.com>
2023-08-03 13:25:28 -0400

    Update Clock.tsx
    - add comments
    - remove use client

--- src/app/components/Clock/Clock.tsx
+++ src/app/components/Clock/Clock.tsx
@@ -1,11 +1,12 @@
-"use client";
 
 import React, { useState, useEffect } from 'react';
 import { ClockIcon } from '@heroicons/react/24/outline';
 
 const Clock: React.FC = () => {
+    // State
     const [dateState, setDateState] = useState<Date>(new Date());
 
+    // Update the date state every 30 seconds.
     useEffect(() => {
         const interval = setInterval(() => setDateState(new Date()), 30000);
         return () => clearInterval(interval);
@@ -13,6 +14,7 @@ const Clock: React.FC = () => {
 
     return (
         <div className="sm:flex sm:items-center sm:justify-end lg:items-end lg:justify-end lg:flex-col h-auto text-white font-bold p-2">
+            {/* Weekday, day, month */}
             <div className="flex justify-end lg:text-xl sm:text-xs xl:mr-16 lg:mr-7">
                 {dateState.toLocaleDateString('en-GB', {
                     weekday: 'short',
@@ -20,6 +22,7 @@ const Clock: React.FC = () => {
                     month: 'short',
                 })}
             </div>
+            {/* Time */}
             <div className="flex items-center justify-end lg:text-5xl sm:text-xs sm:ml-2 xl:mr-16 lg:mr-7 sm:mr-1">
                 <ClockIcon className="sm:hidden lg:inline lg:h-12 lg:w-12 text-white-500" />
                 {dateState.toLocaleString('en-US', {
