04ae34185de56393b94e27c563c62b9cd79ba04f
chilleno <antonion.gonzalezf@gmail.com>
2023-08-04 21:04:52 -0400

    Create Interfaces.tsx
    - add all interfaces in one file

---
+++ src/app/Interfaces.tsx
@@ -0,0 +1,47 @@
+interface Step {
+    header: string;
+    pomodoros: number;
+    currentPomodorosCount: number;
+    assignedTaskList: number;
+    order: number;
+}
+
+interface Task {
+    header: string;
+    checked: boolean;
+    subtasks: any[];
+}
+
+interface TaskList {
+    name: string;
+    tasks: Task[];
+}
+
+interface RoutineProps {
+    setUpdateRoutineStep: (newValue: boolean) => void;
+    updateRoutineStep: boolean;
+    currentTaskIndex: number;
+}
+
+interface NewRoutineStepModalProps {
+    closeModal: () => void;
+    setUpdateRoutineStep: (newValue:boolean) => void;
+    updateRoutineStep: boolean;
+}
+
+interface PomodoroTimerProps {
+    handleCurrentRoutineStepCount: () => void;
+}
+
+interface TaskListProps {
+    currentTaskListIndex: number;
+    nextTaskList(): void;
+    previousTaskList(): void;
+    changeTaskList(taskListIndex: number): void;
+}
+
+interface NewTaskListModalProps {
+    closeModal: () => void;
+    renderList: () => void;
+    handleChangeTaskList: (newValue: number) => void;
+}
