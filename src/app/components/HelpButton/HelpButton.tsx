eac651c68429c69fe269234250abdece7310507a
chilleno <antonion.gonzalezf@gmail.com>
2023-08-05 11:53:00 -0400

    Create HelpButton.tsx
    - create button on bottom right to display help menu.

---
+++ src/app/components/HelpButton/HelpButton.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { QuestionMarkCircleIcon } from '@heroicons/react/24/solid';
+
+const HelpButton: React.FC<QuestionButtonProps> = ({ onClick }) => {
+    return (
+        <QuestionMarkCircleIcon
+            onClick={onClick}
+            className="fixed bottom-4 right-4 w-12 h-12 flex justify-center items-center cursor-pointer"
+        />
+    );
+};
+
+export default HelpButton;
