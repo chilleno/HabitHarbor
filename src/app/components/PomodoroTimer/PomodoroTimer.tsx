d0818eaa6dd19d9c2c81b18128427d22f6993844
chilleno <antonion.gonzalezf@gmail.com>
2023-08-09 18:42:09 -0400

    Update PomodoroTimer.tsx
    - change yellow color to green

--- src/app/components/PomodoroTimer/PomodoroTimer.tsx
+++ src/app/components/PomodoroTimer/PomodoroTimer.tsx
@@ -325,7 +325,7 @@ const PomodoroTimer: React.FC<PomodoroTimerProps> = ({ handleCurrentRoutineStepC
             </div>
             <div className="flex flex-col justify-center content-center">
                 <div className="flex content-center justify-center text-main-primary">
-                    <div className={`rounded-full w-[180px] h-[180px] ${(isBreak == true ? (isLongBreak == true ? 'bg-pomodoro-green' : isShortBreak == true ? 'bg-pomodoro-yellow' : null) : 'bg-pomodoro-red')}`}>
+                    <div className={`rounded-full w-[180px] h-[180px] ${(isBreak == true ? (isLongBreak == true ? 'bg-pomodoro-green' : isShortBreak == true ? 'bg-pomodoro-green' : null) : 'bg-pomodoro-red')}`}>
                         <div className="flex flex-col items-center h-full justify-center leading-10 -mt-4">
                             <div className="text-[16px]">
                                 {(isBreak == true ? (isLongBreak == true ? 'Long break' : isShortBreak == true ? 'Short break' : null) : 'Pomodoro')}
