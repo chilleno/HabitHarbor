53cec34088c6ce9341a6b8e1c64a7e3c863bcb44
chilleno <antonion.gonzalezf@gmail.com>
2023-08-10 23:36:14 -0400

    Update middleScreen.tsx
    - fix sizes

--- src/app/layoutComponents/middleScreen.tsx
+++ src/app/layoutComponents/middleScreen.tsx
@@ -9,7 +9,7 @@ export const useMiddleScreen = () => {
 
 const MiddleScreen: React.FC<{ className: string, children: ReactNode }> = ({ className, children }) => {
   return (
-    <div className={`lg:basis-4/6 sm:h-2/6 lg:h-screen ${className}`}>
+    <div className={`xl:w-4/12 lg:w-4/12 sm:h-2/6 lg:h-screen ${className}`}>
       {children}
     </div>
   );
