d8bad72e77e0d3318d599fa177afa1d9107ffa8d
chilleno <antonion.gonzalezf@gmail.com>
2023-08-10 23:35:24 -0400

    Create NewTaskListModal.tsx
    - new task list modal

---
+++ src/app/components/TaskListSelector/components/NewTaskListModal.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+
+const NewTaskListModal: React.FC<NewTaskListModalProps> = ({ closeModal, renderList, handleChangeTaskList }) => {
+    const [name, setName] = useState('');
+
+    const createNewTaskList = (): void => {
+        if (typeof window !== 'undefined') {
+            const currentTaskLists = JSON.parse(localStorage.getItem('taskLists') || '[]');
+            const newTaskList: TaskList = {
+                name: name,
+                tasks: []
+            }
+            currentTaskLists.push(newTaskList);
+            localStorage.setItem('taskLists', JSON.stringify(currentTaskLists))
+            handleChangeTaskList(currentTaskLists.length - 1)
+            renderList();
+            closeModal();
+        }
+    }
+
+    return (
+        <div className="fixed top-0 left-0 w-full h-full bg-main-primary bg-opacity-90 flex items-center justify-center z-50">
+            <div className="bg-black p-4 rounded-3xl shadow w-auto sm:w-80 text-white border-[2px] border-white">
+                <h2 className="text-xl font-bold mb-4">New Task List</h2>
+                <div className="flex flex-col">
+                    <input
+                        type='text'
+                        placeholder='Enter New Task List Name'
+                        className="text-main-primary rounded-full py-2 placeholder:px-3 px-3"
+                        value={name} onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+                <div className="flex justify-end mt-4">
+                    <button className="bg-green-500 hover:bg-green-600 px-4 py-2 rounded-full text-white" onClick={createNewTaskList}>
+                        Save
+                    </button>
+                    <button className="bg-red-500 hover:bg-red-600 px-4 py-2 rounded-full text-white" onClick={closeModal}>
+                        Close
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NewTaskListModal;
