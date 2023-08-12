3b092629615bb9386fcbfcae56f068ba8aef0d87
chilleno <antonion.gonzalezf@gmail.com>
2023-08-11 20:20:05 -0400

    Create OptionList.tsx
    - add option list for todoTasks component

---
+++ src/app/components/TodoTasks/components/OptionList.tsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+const OptionList: React.ForwardRefRenderFunction<HTMLDivElement, TodoTasksOptionListProps> = ({ onClose }) => {
+    const listRef = useRef<HTMLDivElement>(null);
+
+    const handleClickOutside = (event: MouseEvent) => {
+        if (listRef.current && !listRef.current.contains(event.target as Node)) {
+            onClose();
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
+    return (
+        <div ref={listRef} className="absolute mt-6 ml-36 bg-main-primary border-2 w-50 rounded-lg shadow-md">
+            <div onClick={() => console.log('soon')} className="px-4 py-2 hover:bg-white hover:text-main-primary cursor-pointer justify-start flex">
+                no options
+            </div>
+        </div>
+    );
+};
+
+export default OptionList;
