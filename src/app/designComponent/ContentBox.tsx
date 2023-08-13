137bf53905c14e70624cf794faf14b10b25a3718
chilleno <antonion.gonzalezf@gmail.com>
2023-08-13 11:26:52 -0400

    Update ContentBox.tsx
    - remove center content
    - remove justify center

--- src/app/designComponent/ContentBox.tsx
+++ src/app/designComponent/ContentBox.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 const ContentBox: React.FC<{ className?: string, children?: ReactNode }> = ({ className, children }) => {
     return (
-        <div className={`flex flex-col justify-center content-center shadow-main w-fit gap-1 py-3 px-8 rounded-3xl bg-main-light ${className}`}>
+        <div className={`flex flex-col shadow-main w-fit gap-1 py-3 px-8 rounded-3xl bg-main-light ${className}`}>
             {children}
         </div>
     );
