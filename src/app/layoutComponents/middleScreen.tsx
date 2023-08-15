d02947d70b7ac30453e322c12fdabf08cb18f983
chilleno <antonion.gonzalezf@gmail.com>
2023-08-15 17:31:22 -0400

    Update middleScreen.tsx
    - fix responsive

--- src/app/layoutComponents/middleScreen.tsx
+++ src/app/layoutComponents/middleScreen.tsx
@@ -9,7 +9,7 @@ export const useMiddleScreen = () => {
 
 const MiddleScreen: React.FC<{ className: string, children: ReactNode }> = ({ className, children }) => {
   return (
-    <div className={`xl:w-4/12 lg:w-4/12 sm:h-2/6 lg:h-screen ${className}`}>
+    <div className={`xl:w-4/12 lg:w-5/12 sm:h-2/6 lg:h-screen ${className}`}>
       {children}
     </div>
   );
