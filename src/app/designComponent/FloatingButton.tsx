db918fa768d371219b938d7a83405c56595cbe54
chilleno <antonion.gonzalezf@gmail.com>
2023-08-20 15:16:41 -0400

    Update FloatingButton.tsx
    - change button to div to prevent best practices warnings

--- src/app/designComponent/FloatingButton.tsx
+++ src/app/designComponent/FloatingButton.tsx
@@ -2,9 +2,9 @@ import { ReactNode } from 'react';
 
 const FloatingButton: React.FC<{ className?: string, children?: ReactNode, onClick: Function }> = ({ className, children, onClick }) => {
     return (
-        <button className={`flex bg-main-light h-[50px] w-[50px] rounded-full py-3 px-5 shadow-main content-center justify-center ${className}`} onClick={(e) => onClick(e)}>
+        <div className={`flex bg-main-light h-[50px] w-[50px] rounded-full py-3 px-5 shadow-main content-center justify-center ${className}`} onClick={(e) => onClick(e)}>
             {children}
-        </button>
+        </div>
     );
 };
 
