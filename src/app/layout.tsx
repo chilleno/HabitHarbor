1534e55f21ac768cb423ac03a0d2237eae004b84
chilleno <antonion.gonzalezf@gmail.com>
2023-08-20 15:15:21 -0400

    Update layout.tsx
    - update name to Habit Harbor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Analytics } from '@vercel/analytics/react';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Chaintask',
+  title: 'Habit Harbor',
   description: 'App inspired by Atomic Habits book',
 }
 
