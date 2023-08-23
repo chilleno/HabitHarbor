d5b8b012c897b4f909cf56724df4123697690baf
chilleno <antonion.gonzalezf@gmail.com>
2023-08-22 21:21:08 -0400

    Update endScreen.tsx
    - fix responsive

--- src/app/layoutComponents/endScreen.tsx
+++ src/app/layoutComponents/endScreen.tsx
@@ -9,7 +9,7 @@ export const useEndScreen = () => {
 
 const EndScreen: React.FC<{ className: string, children: ReactNode }> = ({ className, children }) => {
   return (
-    <div className={`xl:w-4/12 lg:w-4/12 sm:h-2/6 xl:h-screen lg:h-screen max-h-[90%] ${className}`}>
+    <div className={`xl:w-4/12 lg:w-4/12 md:w-4/12 sm:h-2/6 ${className}`}>
       {children}
     </div>
   );
