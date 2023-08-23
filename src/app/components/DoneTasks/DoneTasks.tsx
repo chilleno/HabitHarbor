f5c1bb5e27485743ca1d9c513cabd21e73326731
chilleno <antonion.gonzalezf@gmail.com>
2023-08-22 21:21:15 -0400

    Update DoneTasks.tsx
    - fix responsive

--- src/app/components/DoneTasks/DoneTasks.tsx
+++ src/app/components/DoneTasks/DoneTasks.tsx
@@ -84,7 +84,7 @@ const DoneTasks: React.FC<TasksProps> = ({ currentTaskListIndex, taskList, updat
     };
 
     return (
-        <ContentBox className="min-w-full -mt-4 task-list-done">
+        <ContentBox className="min-w-full xl:max-h-[14rem] xl:min-h-[14rem] lg:max-h-[10.3rem] lg:min-h-[10.3rem] md:max-h-[14rem] md:min-h-[14rem] -mt-4 task-list-done">
             <div className="flex justify-end -mr-12 -mt-8">
                 <FloatingButton onClick={() => setShowOptions(!showOptions)}>
                     <span className="flex items-center justify-center hover:cursor-pointer">
@@ -129,7 +129,7 @@ const DoneTasks: React.FC<TasksProps> = ({ currentTaskListIndex, taskList, updat
                                 />
                             </div>
                         </div>
-                    )) : <i>no tasks created...</i>
+                    )) : <i className="text-center pt-10">no tasks created...</i>
                 }
             </div>
         </ContentBox>
