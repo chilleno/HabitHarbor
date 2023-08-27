4ecce80b2408ee20d9e4bb332727a66c29a1feda
chilleno <antonion.gonzalezf@gmail.com>
2023-08-27 19:45:27 -0400

    fix: modal background color

--- src/app/components/PomodoroTimer/PomodoroTimer.tsx
+++ src/app/components/PomodoroTimer/PomodoroTimer.tsx
@@ -359,7 +359,7 @@ const PomodoroTimer: React.FC<PomodoroTimerProps> = ({ handleCurrentRoutineStepC
                 </div>
             </div>
             {showModal && (
-                <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-90 flex items-center justify-center z-50">
+                <div className="fixed top-0 left-0 w-full h-full bg-main-primary bg-opacity-90 flex items-center justify-center z-50">
                     <div className="bg-main-primary p-4 rounded-3xl shadow w-auto sm:w-80 text-white border-[2px] border-white">
                         <h2 className="text-xl font-bold mb-4">Edit Durations</h2>
                         <div className="flex flex-col gap-2">
