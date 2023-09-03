f83b7ed07b43606dd17f4578cf4406bc96ecd920
chilleno <antonion.gonzalezf@gmail.com>
2023-09-03 16:59:38 -0300

    feat: add tailwind configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,16 @@ module.exports = withAnimations({
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily: {
+      "pt-serif": ["PT Serif", "serif"],
+      montserrat: ["Montserrat", "sans-serif"],
+    },
+    backgroundSize: {
+      auto: "auto",
+      cover: "cover",
+      contain: "contain",
+      "100%": "100%",
+    },
     screens: {
       sm: '320px',
       md: '428px',
@@ -15,35 +25,55 @@ module.exports = withAnimations({
       xl: '1440px',
     },
     colors: {
-      'main': {
+      secondary: "#F4F2ED",
+      black: "black",
+      white: "white",
+      main: {
         light: '#1B1A20',
         primary: '#18161A',
       },
-      'pomodoro': {
+      pomodoro: {
         red: '#ED665C',
         yellow: '#F0DB5B',
         green: '#4FF08D',
       },
-      'water': {
+      water: {
         DEFAULT: '#68A0CA',
       },
-      'main': {
+      main: {
         light: '#1B1A20',
         primary: '#18161A',
       },
-      'white': {
-        DEFAULT: '#FFFFFF ',
-      },
-      'gray': {
+      gray: {
         DEFAULT: '#3d3e42',
       },
     },
     extend: {
+      keyframes: {
+        "fade-in-down": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+      },
+      animation: {
+        "fade-in-down": "fade-in-down 0.5s ease-out",
+      },
       boxShadow: {
         'main': '10px 10px 4px 0px rgba(0,0,0,0.25)',
         'habit': '1px 1px 4px 0px rgba(0,0,0,0.25)',
       },
       backgroundImage: {
+        underline1: "url('./assets/Underline1.svg')",
+        underline2: "url('./assets/Underline2.svg')",
+        underline3: "url('./assets/Underline3.svg')",
+        underline4: "url('./assets/Underline4.svg')",
+        highlight3: "url('./assets/Highlight3.svg')",
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
