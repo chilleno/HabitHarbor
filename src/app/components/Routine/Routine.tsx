23f25dbc7eba8640aaded14071d029670d6138d6
chilleno <antonion.gonzalezf@gmail.com>
2023-09-10 01:17:18 -0300

    fix: responsive on lg screen

--- src/app/components/Routine/Routine.tsx
+++ src/app/components/Routine/Routine.tsx
@@ -119,7 +119,7 @@ const Routine: React.FC<RoutineProps> = ({ setUpdateRoutineStep, updateRoutineSt
 
     return (
         <>
-            <ContentBox className="min-w-[400px] xl:min-h-[39rem] xl:max-h-[39rem] lg:min-h-[26rem] lg:max-h-[26rem] md:min-h-[26rem] md:max-h-[26rem] routine">
+            <ContentBox className="xl:min-w-[25rem] lg:min-w-[18rem] lg:max-w-[18rem] md:min-w-[18rem] xl:min-h-[39rem] xl:max-h-[39rem] lg:min-h-[26rem] lg:max-h-[26rem] md:min-h-[26rem] md:max-h-[26rem] routine">
                 <div className="flex justify-end -mr-12 -mt-8">
                     <FloatingButton onClick={() => setShowOptions(!showOptions)}>
                         <span className="flex items-center justify-center hover:cursor-pointer">
@@ -138,9 +138,9 @@ const Routine: React.FC<RoutineProps> = ({ setUpdateRoutineStep, updateRoutineSt
                 <div className="flex justify-center font-bold mb-2 -mt-6">
                     <h1>Routine</h1>
                 </div>
-                <div className="xl:max-h-fit pb-3 xl:h-fit max-h-[64vh] overflow-y-auto flex flex-col px-4 no-scrollbar">
+                <div className="xl:max-h-fit pb-3 xl:h-fit max-h-[64vh] overflow-y-auto flex flex-col no-scrollbar min-w-[16rem]">
                     <ArcherContainer>
-                        <div className="flex flex-col items-center gap-5 max-w-[300px]">
+                        <div className="flex flex-col items-center gap-5 xl:max-w-[18rem] xl:min-w-[18rem] lg:max-w-[14rem] lg:min-w-[14rem]">
                             {routine.map((step, index) => (
                                 <ArcherElement
                                     key={'step_arrow_' + index}
@@ -156,7 +156,7 @@ const Routine: React.FC<RoutineProps> = ({ setUpdateRoutineStep, updateRoutineSt
                                 >
                                     <div
                                         key={'step_box_' + index}
-                                        className={`flex w-full min-w-full bg-main-primary rounded-xl p-3 ${index === currentStep && 'border-2 border-white'}`}
+                                        className={`xl:ml-10 flex w-full xl:min-w-[19rem] xl:max-w-[19rem] lg:min-w-[14rem] lg:max-w-[14rem] bg-[#323333] rounded-xl p-3 ${index === currentStep && 'border-2 border-white'}`}
                                         ref={(ref) => {
                                             taskRefs.current[index] = ref;
                                         }}
@@ -167,15 +167,15 @@ const Routine: React.FC<RoutineProps> = ({ setUpdateRoutineStep, updateRoutineSt
                                                 </div>
                                             </div>
                                         </div>
-                                        <div className="flex flex-col w-8/12 ml-1">
-                                            <h1 className="text-gray font-bold text-sm">Work</h1>
-                                            <h1 className="text-white font-bold text-md mb-1">{step.header}</h1>
-                                            <h1 className="text-white font-bold text-xs max-w-fit py-1 px-3 rounded-2xl bg-[#73F1F3]/20">{getTaskListByIndex(step.assignedTaskList)}</h1>
+                                        <div className="flex flex-col w-6/12 ml-1">
+                                            <h1 className="text-white font-bold xl:text-sm lg:text-xs md:text-xs">Work</h1>
+                                            <h1 className="text-white font-bold xl:text-md lg:text-xs  mb-1">{step.header}</h1>
+                                            <h1 className="text-white font-bold xl:text-xs lg:text-[0.6rem] max-w-fit py-1 px-2 rounded-2xl bg-[#73F1F3]/20">{getTaskListByIndex(step.assignedTaskList)}</h1>
                                         </div>
-                                        <div className="w-2/12 text-white lg:text-xs flex justify-center content-center py-2">
+                                        <div className="w-3/12 text-white xl:text-md lg:text-xs flex justify-center content-center py-2">
                                             <b>{step.currentPomodorosCount + ' / ' + step.pomodoros}</b>
                                         </div>
-                                        <FloatingButton className="absolute right-0 -mr-1 mt-10 bg-main-primary" onClick={() => setShowOptionStep(index)}>
+                                        <FloatingButton className="absolute right-0 xl:mr-1 lg:mr-3 xl:mt-10 lg:mt-8 bg-main-primary" onClick={() => setShowOptionStep(index)}>
                                             <span className="flex items-center justify-center hover:cursor-pointer">
                                                 <CogIcon className="h-[24px] w-[24px] text-white" />
                                             </span>
