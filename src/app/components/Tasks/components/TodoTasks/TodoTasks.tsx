7044ba04acebc7f9d81accf8c6e5bc68d5552683
chilleno <antonion.gonzalezf@gmail.com>
2023-11-24 11:49:56 -0300

    feat: remove new task input and hot task

--- src/app/components/Tasks/components/TodoTasks/TodoTasks.tsx
+++ src/app/components/Tasks/components/TodoTasks/TodoTasks.tsx
@@ -8,12 +8,11 @@ import PrioritizeModal from './components/PrioritizeModal';
 import useSound from 'use-sound';
 
 
-const TodoTasks: React.FC<TasksProps> = ({ currentTaskListIndex, taskList, updateTaskList, setUpdateTaskList }) => {
+const TodoTasks: React.FC<TasksProps> = ({ currentTaskListIndex, taskList, updateTaskList, setUpdateTaskList, highlightedTask, setHighlightedTask }) => {
     const [newTask, setNewTask] = useState<string>('');
     const timeoutRef = useRef<NodeJS.Timeout | null>(null);
     const [showError, setShowError] = useState<boolean>(false);
     const [showOptions, setShowOptions] = useState<boolean>(false);
-    const [highlightedTask, setHighlightedTask] = useState<number | null>(null);
     const [showPrioritizeModal, setShowPrioritizeModal] = useState<boolean>(false);
 
     //sfx
@@ -238,68 +237,6 @@ const TodoTasks: React.FC<TasksProps> = ({ currentTaskListIndex, taskList, updat
                     }
                 </FloatingButton>
             </div>
-            <div className="flex justify-center items-center font-bold -mt-6 mb-2">
-                <h1 className="text-white xl:text-xl lg:text-md md:tex-md">Tasks</h1>
-            </div>
-            <div className="flex flex-col justify-center content-center">
-                <InputText
-                    placeholder="Type and press enter to create a task..."
-                    value={newTask}
-                    id="newTaskInput"
-                    onChange={(value) => setNewTask(value)}
-                    onKeyDown={handlePressEnterButton}
-                    className="focus:ring-0 focus:border-main-primary xl:text-lg lg:text-xs md:text-xs"
-                    name="task-name-new"
-                />
-                <b className={`xl:text-md lg:text-xs md:text-xs ml-5 text-[red] transition-opacity duration-150 ${showError ? 'opacity-100 animate-headShake' : 'opacity-0'}`}>
-                    <i>Please add a text longer than 3 characters.</i>
-                </b>
-            </div>
-            {
-                (highlightedTask !== null && taskList[highlightedTask]) &&
-                <div className="flex gap-5 ">
-                    <div className="flex ml-1 xl:text-lg lg:text-xs md:text-xs w-2/12 animate-backInLeft gap-3">
-                        <div className="animate-heartBeat animate-infinite ">
-                            🔥
-                        </div>
-                        Focus on
-                        <div className="animate-heartBeat animate-infinite ">
-                            👉
-                        </div>
-                    </div>
-                    <div
-                        className={`gap-1 group/item flex items-center bg-main-primary xl:mb-2 lg:mb-1 min-h-[32px] h-[32px] max-h-[32px] text-start py-3 px-3 rounded-3xl animate-backInUp w-10/12`}
-                    >
-                        <input
-                            checked={taskList[highlightedTask].checked || false}
-                            onChange={() => handleCheckboxChange(highlightedTask)}
-                            type='checkbox'
-                            className="w-[20px] h-[20px] rounded-3xl focus:rounded-full bg-main-primary border-[#3D3E42] border-2 shadow-sm focus:ring focus:ring-indigo-200 focus:ring-opacity-50 "
-                        />
-                        <div className="animate-heartBeat animate-delay-75 animate-infinite ml-2 xl:text-lg lg:text-xs md:text-xs">
-                            {taskList[highlightedTask].priority}
-                        </div>
-                        <input
-                            className="xl:text-lg lg:text-xs md:text-xs lg:placeholder:text-xs ml-2 w-full bg-[transparent] border-0 focus:ring-0 focus:border-b-2 focus:border-white"
-                            style={{ textDecoration: taskList[highlightedTask].checked ? 'line-through' : 'none' }}
-                            defaultValue={taskList[highlightedTask].header}
-                            onBlur={(e) => handleChangeTaskText(e.target.value, highlightedTask, e)}
-                        />
-                        <div className="invisible group-hover/item:visible w-1/12 flex justify-end mr-3 gap-3">
-                            <b
-                                onClick={() => handleHighlightTask(null)}
-                                className="text-xs text-white hover:cursor-pointer"
-                            >
-                                🧯
-                            </b>
-                            <TrashIcon
-                                onClick={() => handleDeleteTask(highlightedTask)}
-                                className="h-4 w-4 text-white hover:cursor-pointer"
-                            />
-                        </div>
-                    </div>
-                </div>
-            }
             <div className="flex flex-col h-fit">
                 {
                     taskList && taskList.length > 0 ? taskList.map((task, index) => (
