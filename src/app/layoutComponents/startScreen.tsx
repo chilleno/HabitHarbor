8c26edd08e42999327ba2256f3e5b1baa9fa3a69
chilleno <antonion.gonzalezf@gmail.com>
2023-11-24 16:38:50 -0300

    feat: implement border

--- src/app/layoutComponents/startScreen.tsx
+++ src/app/layoutComponents/startScreen.tsx
@@ -9,7 +9,7 @@ export const useStartScreen = () => {
 
 const StartScreen: React.FC<{ className: string, children: ReactNode }> = ({ className, children }) => {
   return (
-    <div className={`w-3/12 fixed inset-y-0 left-0 ${className}`}>
+    <div className={`w-3/12 fixed border-r-2 border-gray inset-y-0 left-0 ${className}`}>
       {children}
     </div>
   );
