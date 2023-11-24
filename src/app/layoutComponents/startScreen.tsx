7ff11d6023b4325da1b0d8d80eec67993c0682f5
chilleno <antonion.gonzalezf@gmail.com>
2023-11-23 23:45:47 -0300

    feat: fix on the left

--- src/app/layoutComponents/startScreen.tsx
+++ src/app/layoutComponents/startScreen.tsx
@@ -9,7 +9,7 @@ export const useStartScreen = () => {
 
 const StartScreen: React.FC<{ className: string, children: ReactNode }> = ({ className, children }) => {
   return (
-    <div className={`w-3/12  ${className}`}>
+    <div className={`w-3/12 fixed inset-y-0 left-0 ${className}`}>
       {children}
     </div>
   );
