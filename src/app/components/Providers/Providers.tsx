36ff59ba80e00a4f86508dc85172008db00fceea
chilleno <antonion.gonzalezf@gmail.com>
2023-12-05 18:05:39 -0300

    feat: add auth provider

---
+++ src/app/components/Providers/Providers.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { SessionProvider } from "next-auth/react";
+import React, { ReactNode } from "react";
+
+interface ProviderProps {
+    children: ReactNode;
+}
+
+const Providers = (props: ProviderProps) => {
+    return <SessionProvider>{props.children}</SessionProvider>;
+}
+
+export default Providers;
