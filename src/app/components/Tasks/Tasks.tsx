a16717f10723ec2bf84c9e8160b62e61783086c6
chilleno <antonion.gonzalezf@gmail.com>
2023-12-14 22:12:20 -0300

    feat: some mobile updates

--- src/app/components/Tasks/Tasks.tsx
+++ src/app/components/Tasks/Tasks.tsx
@@ -138,7 +138,7 @@ const Tasks: React.FC<TaskListProps> = ({ taskList, currentTaskListIndex, change
 
     return (
         <div className="min-w-full task-list-selector min-h-screen">
-            <div className="fixed min-w-[50%] bg-[#323333] border-b-2 border-gray z-40">
+            <div className="fixed min-w-[50%] max-w-[50%] bg-[#323333] border-b-2 border-gray z-40">
                 {
                     !true && <div className="flex flex-grid justify-center items-center font-bold py-4 border-b-2 border-gray gap-5">
                         <h1 className="text-white xl:text-xl lg:text-md xl:w-2/12 lg:w-2/12">Task lists </h1>
@@ -247,7 +247,7 @@ const Tasks: React.FC<TaskListProps> = ({ taskList, currentTaskListIndex, change
                     lists={lists}
                 />
             </div>
-            <div className={(highlightedTask === null || highlightedTask < 0) ? "xl:pt-[10%] lg:pt-[14%] px-12" : "xl:pt-[15%] lg:pt-[20%] px-12"}>
+            <div className={(highlightedTask === null || highlightedTask < 0) ? "xl:pt-[10%] lg:pt-[14%] md:pt-[19%] px-12" : "xl:pt-[15%] lg:pt-[20%] md:pt-[29%] px-12"}>
                 {
                     moveTasksMode === false &&
                     <>
