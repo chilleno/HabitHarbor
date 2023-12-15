41ef3e870ae0539f643e304668b71d2db1097b4d
chilleno <antonion.gonzalezf@gmail.com>
2023-12-15 15:54:36 -0300

    feat: change text to icon on logout button

--- src/app/components/SigninButton/SigninButton.tsx
+++ src/app/components/SigninButton/SigninButton.tsx
@@ -1,6 +1,8 @@
 "use client";
 import React from 'react';
 import { signIn, signOut, useSession } from 'next-auth/react';
+import { ArrowLeftOnRectangleIcon } from '@heroicons/react/24/solid';
+import { Tooltip as ReactTooltip } from "react-tooltip";
 
 const SigninButton = () => {
     const { data: session } = useSession();
@@ -11,17 +13,22 @@ const SigninButton = () => {
             <div className="flex gap-3">
                 {
                     session.user.image &&
-                    <div className="xl:text-lg lg:text-xs w-2/12 flex items-center">
+                    <div className="xl:text-lg lg:text-xs md:text-xs w-1/12 flex items-center">
                         <img width="50" height="50" className="rounded-full" src={session.user.image} alt="user_profile_image" />
                     </div>
                 }
-                <span className="xl:text-lg lg:text-xs w-7/12 flex items-center"> <b className="">{session.user.name}</b></span>
-                <div className="w-3/12 flex items-center justify-end">
-                    <button className="py-2 px-2 text-black hover:text-white bg-white hover:bg-black border-2 border-black hover:border-white rounded-3xl xl:text-lg lg:text-xs"
+                <span className="xl:text-lg lg:text-xs md:text-xs w-8/12 flex items-center"> <b className="">{session.user.name}</b></span>
+                <div className="w-1/12 flex items-center justify-end">
+                    <button className="py-2 px-2 text-black hover:text-white bg-white hover:bg-black border-2 border-black hover:border-white rounded-3xl xl:text-lg lg:text-xs" data-tooltip-id="logoutButton" 
                         onClick={() => { signOut({ callbackUrl: '/' }) }}
                     >
-                        Sign out
+                        <ArrowLeftOnRectangleIcon className="h-5 w-5" />
                     </button>
+                    <ReactTooltip
+                        id="logoutButton"
+                        place="bottom"
+                        content="Logout"
+                    />
                 </div>
             </div>
         )
