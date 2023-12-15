dd2a9a11fb6a6f0ed70720f2cbdb641b93d2cf6b
chilleno <antonion.gonzalezf@gmail.com>
2023-12-15 15:42:25 -0300

    feat: add responsive for mobile

--- src/app/components/Tasks/components/OptionList.tsx
+++ src/app/components/Tasks/components/OptionList.tsx
@@ -1,50 +1,24 @@
 import React, { useState, useEffect, useRef } from 'react';
 
-const OptionList: React.ForwardRefRenderFunction<HTMLDivElement, TaskListOptionListProps> = ({ onClose, openModal, currentSelection, handleChangeTaskList, renderTaskLists }) => {
+
+
+const OptionList: React.FC<TasksCreationOptionList> = ({ openPrioritizeModal, handleMoveTaskMode, deleteAllDoneTasks, deleteAllTasks }) => {
     const listRef = useRef<HTMLDivElement>(null);
-
-    const handleClickOutside = (event: MouseEvent) => {
-        if (listRef.current && !listRef.current.contains(event.target as Node)) {
-            onClose();
-        }
-    };
-
-    const handleDeleteTaskList = (): void => {
-        if (typeof window !== 'undefined') {
-            if (window.confirm('are you sure you want to delete the entire task list?')) {
-                let currentStoredTaskLists = JSON.parse(localStorage.getItem('taskLists') || '[]');
-                let updatedTaskLists = currentStoredTaskLists.filter((taskList, index) => index !== currentSelection);
-
-                localStorage.setItem('taskLists', JSON.stringify(updatedTaskLists));
-
-                // fired custom event on localStorage data changed
-                const event = new CustomEvent('taskListdatachanged') as any;
-                document.dispatchEvent(event);
-
-                renderTaskLists();
-                handleChangeTaskList(updatedTaskLists.length - 1);
-            }
-        }
-    };
-
-    useEffect(() => {
-        document.addEventListener('mousedown', handleClickOutside);
-        return () => {
-            document.removeEventListener('mousedown', handleClickOutside);
-        };
-    }, []);
 
     return (
         <div ref={listRef} className="absolute mt-6 -ml-40 bg-main-primary border-2 w-50 rounded-lg shadow-md">
-            <div onClick={openModal} className="px-4 py-2 hover:bg-white hover:text-main-primary cursor-pointer justify-start flex">
-                New task list
+            <div onClick={openPrioritizeModal} className="px-4 py-2 hover:bg-white hover:text-main-primary cursor-pointer justify-start flex">
+                Prioritize tasks
             </div>
-            {
-                currentSelection >= 0 &&
-                <div onClick={() => handleDeleteTaskList()} className="px-4 py-2 hover:bg-white hover:text-main-primary cursor-pointer justify-start flex">
-                    Delete current task list
-                </div>
-            }
+            <div onClick={handleMoveTaskMode} className="px-4 py-2 hover:bg-white hover:text-main-primary cursor-pointer justify-start flex">
+                Move tasks
+            </div>
+            <div onClick={deleteAllDoneTasks} className="px-4 py-2 hover:bg-white hover:text-main-primary cursor-pointer justify-start flex">
+                Delete finished tasks
+            </div>
+            <div onClick={deleteAllTasks} className="px-4 py-2 hover:bg-white hover:text-main-primary cursor-pointer justify-start flex">
+                Delete ALL tasks
+            </div>
         </div>
     );
 };
