c1b10819d2e88f1bb815a435ade052a9d78aef78
chilleno <antonion.gonzalezf@gmail.com>
2023-12-17 23:28:21 -0300

    feat: import authOptions from new lib

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,42 +1,5 @@
-import NextAuth, { AuthOptions } from "next-auth"
-import { SupabaseAdapter } from "@auth/supabase-adapter"
-import GoogleProvider from "next-auth/providers/google";
-import { Adapter } from "next-auth/adapters";
-import { createClient } from "@supabase/supabase-js"
-
-export const authOptions: AuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-    })
-  ],
-  adapter: SupabaseAdapter({
-    url: process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
-    secret: process.env.SUPABASE_SERVICE_ROLE_KEY ?? "",
-  }) as Adapter,
-  callbacks: {
-    session: async ({ session, token, user }) => {
-      const supabase = createClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-        { db: { schema: 'next_auth' } }
-      );
-
-      const { data, status, error } = await supabase
-        .from('users')
-        .select('profile_id')
-        .eq('id', user.id)
-        .single();
-      
-      if (session?.user) {
-        session.user.id = user.id as string;
-        session.user.profile_id = data?.profile_id as string;
-      }
-      return session;
-    },
-  }
-}
+import NextAuth from "next-auth"
+import { authOptions } from "../../../lib/authOptions";
 
 const handler = NextAuth(authOptions)
 
