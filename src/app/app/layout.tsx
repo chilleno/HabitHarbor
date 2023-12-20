b3f0536b97373b9f77beb6b0ad727b607c336fd0
chilleno <antonion.gonzalezf@gmail.com>
2023-12-20 20:33:07 -0300

    feat: implement login provider on app layout

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react';
 import EventProvider from '../components/Providers/EventProvider';
 import './rotate.css';
+import Providers from '../components/Providers/Providers';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,20 +20,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <EventProvider>
-          <div className="sm:flex xl:hidden lg:hidden md:hidden w-full justify-center content-center min-h-[100vh] items-center">
-            <div className="flex flex-col">
-              <div className="flex pl-12">
-                <div className="phone">
+        <Providers >
+          <EventProvider>
+            <div className="sm:flex xl:hidden lg:hidden md:hidden w-full justify-center content-center min-h-[100vh] items-center">
+              <div className="flex flex-col">
+                <div className="flex pl-12">
+                  <div className="phone">
+                  </div>
+                </div>
+                <div className="message">
+                  Please rotate your device!
                 </div>
               </div>
-              <div className="message">
-                Please rotate your device!
-              </div>
             </div>
-          </div>
-          {children}
-        </EventProvider>
+            {children}
+          </EventProvider>
+        </Providers>
       </body>
       <Analytics />
     </html>
