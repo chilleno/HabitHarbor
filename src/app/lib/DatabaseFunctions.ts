a68b40b40fc7e74890c47db319fa215ca9326d56
chilleno <antonion.gonzalezf@gmail.com>
2023-12-21 19:16:00 -0300

    feat: add get functions

--- src/app/lib/DatabaseFunctions.ts
+++ src/app/lib/DatabaseFunctions.ts
@@ -5,11 +5,26 @@ export const savePomodoro = async (pomodoro: any, user: any): Promise<Boolean> =
         .from('config')
         .upsert({ pomodoro: pomodoro, user_id: user.id, updated_at: new Date() })
         .select()
+        .single()
     if (status === 201) {
         return true;
     } else {
         console.log(error);
         return false;
+    }
+}
+
+export const loadPomodoro = async (user: any): Promise<any> => {
+    const { data, error, status } = await supabase
+        .from('config')
+        .select()
+        .eq('user_id', user.id)
+        .single()
+    if (status === 200) {
+        return { status: 200, data: data.pomodoro };
+    } else {
+        console.log(error);
+        return { status: 420};
     }
 }
 
@@ -18,11 +33,26 @@ export const saveTaskLists = async (taskList: any, user: any): Promise<Boolean> 
         .from('task_lists')
         .upsert({ lists: taskList, user_id: user.id, updated_at: new Date() })
         .select()
+        .single()
     if (status === 201) {
         return true;
     } else {
         console.log(error);
         return false;
+    }
+}
+
+export const loadTaskLists = async (user: any): Promise<any> => {
+    const { data, error, status } = await supabase
+        .from('task_lists')
+        .select()
+        .eq('user_id', user.id)
+        .single()
+    if (status === 200) {
+        return { status: 200, data: data.lists };
+    } else {
+        console.log(error);
+        return { status: 420};
     }
 }
 
@@ -31,6 +61,7 @@ export const saveHabits = async (habits: any, user: any): Promise<Boolean> => {
         .from('habits')
         .upsert({ trackers: habits, user_id: user.id, updated_at: new Date() })
         .select()
+        .single()
     if (status === 201) {
         return true;
     } else {
@@ -39,3 +70,16 @@ export const saveHabits = async (habits: any, user: any): Promise<Boolean> => {
     }
 }
 
+export const loadHabits = async (user: any): Promise<any> => {
+    const { data, error, status } = await supabase
+        .from('habits')
+        .select()
+        .eq('user_id', user.id)
+        .single()
+    if (status === 200) {
+        return { status: 200, data: data.trackers };
+    } else {
+        console.log(error);
+        return { status: 420};
+    }
+}
