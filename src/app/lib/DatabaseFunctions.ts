87b952072228efa2921b15bd34422814b1f69b68
chilleno <antonion.gonzalezf@gmail.com>
2023-12-21 16:07:29 -0300

    feat: database functions

---
+++ src/app/lib/DatabaseFunctions.ts
@@ -0,0 +1,17 @@
+
+
+export const savePomodoro = (pomodoro: any, user: any): boolean => {
+    console.log("savePomodoro", pomodoro);
+    return true;
+}
+
+export const saveTaskLists = (taskList: any, user: any): boolean => {
+    console.log("saveTaskList", taskList);
+    return true;
+}
+
+export const saveHabits = (habits: any, user: any): boolean => {
+    console.log("saveHabits", habits);
+    return true;
+}
+
