0418b21f0dabe998e65fe215bbcf3cfc5fe0f19a
chilleno <antonion.gonzalezf@gmail.com>
2023-12-23 15:08:29 -0300

    fix: restore needed element

---
+++ src/app/designComponent/FloatingButton.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react';
+
+const FloatingButton: React.FC<{ className?: string, children?: ReactNode, onClick: Function }> = ({ className, children, onClick }) => {
+    return (
+        <div className={`flex bg-main-light h-[50px] w-[50px] rounded-full py-3 px-5 shadow-main content-center justify-center ${className}`} onClick={(e) => onClick(e)}>
+            {children}
+        </div>
+    );
+};
+
+export default FloatingButton;
