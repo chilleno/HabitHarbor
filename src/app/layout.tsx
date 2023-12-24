c1e72982f77be65b221d329bc2e875ea0f7efe0d
chilleno <antonion.gonzalezf@gmail.com>
2023-12-23 23:19:07 -0300

    feat: remove attr from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,8 +16,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className="scroll-smooth scroll-p-[20rem]">
-      <body className={inter.className}>
+    <html>
+      <body>
         <Providers>
           {children}
         </Providers>
