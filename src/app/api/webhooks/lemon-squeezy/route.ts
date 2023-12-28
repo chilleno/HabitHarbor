bffb95b59411fc2314f03175713c9facccd7ca0d
chilleno <antonion.gonzalezf@gmail.com>
2023-12-28 19:48:28 -0300

    feat: update hook

--- src/app/api/webhooks/lemon-squeezy/route.ts
+++ src/app/api/webhooks/lemon-squeezy/route.ts
@@ -1,37 +1,32 @@
 import type { NextApiResponse } from 'next'
 
+enum WebhookEvent {
+  subscriptionCreated = 'subscription_created',
+  subscriptionUpdated = 'subscription_updated',
+  subscriptionPaymentSuccess = 'subscription_payment_success',
+  subscriptionExpired = 'subscription_expired',
+}
 
 const handler = async (
   req: Request,
   res: NextApiResponse
 ) => {
-  console.log("Webhook hit")
   const crypto = require('crypto');
   const rawBody = await req.text()
-  //get secret from env
-  console.log("1");
   const secret = process.env.LEMONSQUEEZY_SIGNING_SECRET;
-  console.log("2");
   const hmac = crypto.createHmac('sha256', secret);
-  console.log("3");
   const digest = Buffer.from(hmac.update(rawBody).digest('hex'), 'utf8');
-  console.log("4");
   const signature = Buffer.from(req.headers.get('X-Signature') || '', 'utf8');
-  console.log("5");
   
   if (!crypto.timingSafeEqual(digest, signature)) {
-    console.log("6");
     throw new Error('Invalid signature.');
   }
 
-  console.log('Valid signature.');
   const data = JSON.parse(rawBody)
+  console.log(data)
   const eventName = data && data['meta']['event_name']
   const obj = data && data['data']['attributes']
   const objId = data && data['data']['id']
-  console.log(eventName)
-  console.log(obj)
-  console.log(objId)
 
   return new Response(JSON.stringify({ code: 200, message: "Success" }));
 }
