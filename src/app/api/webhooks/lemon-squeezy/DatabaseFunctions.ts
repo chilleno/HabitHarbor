3dea1aad6fca91f173e82aa0085c1ea33789e327
chilleno <antonion.gonzalezf@gmail.com>
2023-12-29 22:30:33 -0300

    feat: fix insert invoice

--- src/app/api/webhooks/lemon-squeezy/DatabaseFunctions.ts
+++ src/app/api/webhooks/lemon-squeezy/DatabaseFunctions.ts
@@ -46,7 +46,38 @@ export const updateProfileSubscription = async (SubscriptionObject: Subscription
 export const createInvoice = async (SubscriptionInvoiceObject: SubscriptionInvoiceObject, userId: string): Promise<any> => {
     const { data, error, status } = await supabase
         .from('invoices')
-        .insert({ ...SubscriptionInvoiceObject.attributes, userId })
+        .insert({ 
+            user_id: userId,
+            store_id: SubscriptionInvoiceObject.attributes.store_id,
+            subscription_id: SubscriptionInvoiceObject.attributes.subscription_id,
+            customer_id: SubscriptionInvoiceObject.attributes.customer_id,
+            user_name: SubscriptionInvoiceObject.attributes.user_name,
+            user_email: SubscriptionInvoiceObject.attributes.user_email,
+            billing_reason: SubscriptionInvoiceObject.attributes.billing_reason,
+            card_brand: SubscriptionInvoiceObject.attributes.card_brand,
+            card_last_four: SubscriptionInvoiceObject.attributes.card_last_four,
+            currency: SubscriptionInvoiceObject.attributes.currency,
+            currency_rate: SubscriptionInvoiceObject.attributes.currency_rate,
+            status: SubscriptionInvoiceObject.attributes.status,
+            status_formatted: SubscriptionInvoiceObject.attributes.status_formatted,
+            refunded: SubscriptionInvoiceObject.attributes.refunded,
+            refunded_at: SubscriptionInvoiceObject.attributes.refunded_at,
+            subtotal: SubscriptionInvoiceObject.attributes.subtotal,
+            discount_total: SubscriptionInvoiceObject.attributes.discount_total,
+            tax: SubscriptionInvoiceObject.attributes.tax,
+            total: SubscriptionInvoiceObject.attributes.total,
+            subtotal_usd: SubscriptionInvoiceObject.attributes.subtotal_usd,
+            discount_total_usd: SubscriptionInvoiceObject.attributes.discount_total_usd,
+            tax_usd: SubscriptionInvoiceObject.attributes.tax_usd,
+            total_usd: SubscriptionInvoiceObject.attributes.total_usd,
+            subtotal_formatted: SubscriptionInvoiceObject.attributes.subtotal_formatted,
+            discount_total_formatted: SubscriptionInvoiceObject.attributes.discount_total_formatted,
+            tax_formatted: SubscriptionInvoiceObject.attributes.tax_formatted,
+            total_formatted: SubscriptionInvoiceObject.attributes.total_formatted,
+            urls: SubscriptionInvoiceObject.attributes.urls,
+            created_at: SubscriptionInvoiceObject.attributes.created_at,
+            updated_at: SubscriptionInvoiceObject.attributes.updated_at,
+         })
     if (status === 200) {
         return { status: 200 };
     } else {
