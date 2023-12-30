27d196e28690f2c045dd49926a50bddfe1fc8944
chilleno <antonion.gonzalezf@gmail.com>
2023-12-29 21:56:07 -0300

    feat: fix

--- src/app/api/webhooks/lemon-squeezy/route.ts
+++ src/app/api/webhooks/lemon-squeezy/route.ts
@@ -34,130 +34,136 @@ const handler = async (
 
   if (eventName === WebhookEvent.orderCreated) {
     const order: OrderObject = data;
-    const updated = await updateProfileOrder(order, customData.user_id);
-    console.log('order created');
-    console.log(order);
-    if (updated === true) {
-      const newInvoice: SubscriptionInvoiceObject = {
-        type: "founder_invoice",
-        id: objId,
-        attributes: {
-          store_id: order.attributes.store_id,
-          subscription_id: objId,
-          customer_id: order.attributes.customer_id,
-          user_name: order.attributes.user_name,
-          user_email: order.attributes.user_email,
-          billing_reason: 'founder habbit harbor',
-          card_brand: 'not informed',
-          card_last_four: 'not informed',
-          currency: order.attributes.currency,
-          currency_rate: order.attributes.currency_rate,
-          status: order.attributes.status,
-          status_formatted: order.attributes.status_formatted,
-          refunded: false,
-          refunded_at: null,
-          subtotal: order.attributes.total,
-          discount_total: order.attributes.discount_total,
-          tax: order.attributes.tax,
-          total: order.attributes.total,
-          subtotal_usd: order.attributes.total_usd,
-          discount_total_usd: order.attributes.discount_total_usd,
-          tax_usd: order.attributes.tax_usd,
-          total_usd: order.attributes.total_usd,
-          subtotal_formatted: order.attributes.total_formatted,
-          discount_total_formatted: order.attributes.discount_total_formatted,
-          tax_formatted: order.attributes.tax_formatted,
-          total_formatted: order.attributes.total_formatted,
-          urls: order.attributes.urls,
-          created_at: order.attributes.created_at,
-          updated_at: order.attributes.updated_at,
-          test_mode: order.attributes.test_mode,
-        },
+    if (order.attributes.first_order_item.variant_id === 191246) {
+      if (order.attributes.status === 'paid') {
+        const updated = await updateProfileOrder(order, customData.user_id);
+        console.log('order created');
+        console.log(order);
+        if (updated === true) {
+          const newInvoice: SubscriptionInvoiceObject = {
+            type: "founder_invoice",
+            id: objId,
+            attributes: {
+              store_id: order.attributes.store_id,
+              subscription_id: objId,
+              customer_id: order.attributes.customer_id,
+              user_name: order.attributes.user_name,
+              user_email: order.attributes.user_email,
+              billing_reason: 'founder habbit harbor',
+              card_brand: 'not informed',
+              card_last_four: 'not informed',
+              currency: order.attributes.currency,
+              currency_rate: order.attributes.currency_rate,
+              status: order.attributes.status,
+              status_formatted: order.attributes.status_formatted,
+              refunded: false,
+              refunded_at: null,
+              subtotal: order.attributes.total,
+              discount_total: order.attributes.discount_total,
+              tax: order.attributes.tax,
+              total: order.attributes.total,
+              subtotal_usd: order.attributes.total_usd,
+              discount_total_usd: order.attributes.discount_total_usd,
+              tax_usd: order.attributes.tax_usd,
+              total_usd: order.attributes.total_usd,
+              subtotal_formatted: order.attributes.total_formatted,
+              discount_total_formatted: order.attributes.discount_total_formatted,
+              tax_formatted: order.attributes.tax_formatted,
+              total_formatted: order.attributes.total_formatted,
+              urls: order.attributes.urls,
+              created_at: order.attributes.created_at,
+              updated_at: order.attributes.updated_at,
+              test_mode: order.attributes.test_mode,
+            },
+          }
+          const newInvoiceResponse = await createInvoice(newInvoice, customData.user_id);
+
+          if (newInvoiceResponse.status === 200) {
+            console.log('subscription payment success')
+            console.log(newInvoiceResponse);
+
+            return new Response(JSON.stringify({ code: 200, message: "subscription payment success" }));
+
+          } else if (newInvoiceResponse.status === 420) {
+            console.log('Something went wront on request')
+            console.log(newInvoiceResponse);
+            return new Response(JSON.stringify({ code: 420, message: "Something went wrong on request" }));
+          }
+        } else {
+          return new Response(JSON.stringify({ code: 420, message: "Something went wrong on request" }));
+        }
+      } else {
+        return new Response(JSON.stringify({ code: 200, message: "request ok" }));
       }
-      const newInvoiceResponse = await createInvoice(newInvoice, customData.user_id);
+    }
+
+    if (eventName === WebhookEvent.orderRefounded) {
+      const order: OrderObject = data;
+      const updated = await updateProfileOrder(order, customData.user_id);
+      console.log('order refounded');
+      console.log(order);
+      if (updated === true) {
+        return { status: 200, message: "success" };
+      } else {
+        return { status: 420, messsage: "something went wrong" };
+      }
+    }
+
+    if (eventName === WebhookEvent.subscriptionCreated) {
+      const subscription: SubscriptionObject = data;
+      const updated = await updateProfileSubscription(subscription, customData.user_id);
+      console.log('subscription created');
+      console.log(subscription);
+      if (updated === true) {
+        return { status: 200, message: "success" };
+      } else {
+        return { status: 420, messsage: "something went wrong" };
+      }
+    }
+
+    if (eventName === WebhookEvent.subscriptionUpdated) {
+      const subscription: SubscriptionObject = data;
+      const updated = await updateProfileSubscription(subscription, customData.user_id);
+      console.log('subscription created');
+      console.log(subscription);
+      if (updated === true) {
+        return { status: 200, message: "success" };
+      } else {
+        return { status: 420, messsage: "something went wrong" };
+      }
+    }
+
+    if (eventName === WebhookEvent.subscriptionExpired) {
+      const subscription: SubscriptionObject = data;
+      const updated = await updateProfileSubscription(subscription, customData.user_id);
+      console.log('subscription created');
+      console.log(subscription);
+      if (updated === true) {
+        return { status: 200, message: "success" };
+      } else {
+        return { status: 420, messsage: "something went wrong" };
+      }
+    }
+
+    if (eventName === WebhookEvent.subscriptionPaymentSuccess) {
+      const subscriptionInvoice: SubscriptionInvoiceObject = data;
+      const newInvoiceResponse = await createInvoice(subscriptionInvoice, customData.user_id);
 
       if (newInvoiceResponse.status === 200) {
         console.log('subscription payment success')
         console.log(newInvoiceResponse);
 
         return new Response(JSON.stringify({ code: 200, message: "subscription payment success" }));
-        
+
       } else if (newInvoiceResponse.status === 420) {
         console.log('Something went wront on request')
         console.log(newInvoiceResponse);
         return new Response(JSON.stringify({ code: 420, message: "Something went wrong on request" }));
       }
-    } else {
-      return new Response(JSON.stringify({ code: 420, message: "Something went wrong on request" }));
     }
+
+    return new Response(JSON.stringify({ code: 420, message: "Wrong payload" }));
   }
-
-  if (eventName === WebhookEvent.orderRefounded) {
-    const order: OrderObject = data;
-    const updated = await updateProfileOrder(order, customData.user_id);
-    console.log('order refounded');
-    console.log(order);
-    if (updated === true) {
-      return { status: 200, message: "success" };
-    } else {
-      return { status: 420, messsage: "something went wrong" };
-    }
-  }
-
-  if (eventName === WebhookEvent.subscriptionCreated) {
-    const subscription: SubscriptionObject = data;
-    const updated = await updateProfileSubscription(subscription, customData.user_id);
-    console.log('subscription created');
-    console.log(subscription);
-    if (updated === true) {
-      return { status: 200, message: "success" };
-    } else {
-      return { status: 420, messsage: "something went wrong" };
-    }
-  }
-
-  if (eventName === WebhookEvent.subscriptionUpdated) {
-    const subscription: SubscriptionObject = data;
-    const updated = await updateProfileSubscription(subscription, customData.user_id);
-    console.log('subscription created');
-    console.log(subscription);
-    if (updated === true) {
-      return { status: 200, message: "success" };
-    } else {
-      return { status: 420, messsage: "something went wrong" };
-    }
-  }
-
-  if (eventName === WebhookEvent.subscriptionExpired) {
-    const subscription: SubscriptionObject = data;
-    const updated = await updateProfileSubscription(subscription, customData.user_id);
-    console.log('subscription created');
-    console.log(subscription);
-    if (updated === true) {
-      return { status: 200, message: "success" };
-    } else {
-      return { status: 420, messsage: "something went wrong" };
-    }
-  }
-
-  if (eventName === WebhookEvent.subscriptionPaymentSuccess) {
-    const subscriptionInvoice: SubscriptionInvoiceObject = data;
-    const newInvoiceResponse = await createInvoice(subscriptionInvoice, customData.user_id);
-
-    if (newInvoiceResponse.status === 200) {
-      console.log('subscription payment success')
-      console.log(newInvoiceResponse);
-
-      return new Response(JSON.stringify({ code: 200, message: "subscription payment success" }));
-
-    } else if (newInvoiceResponse.status === 420) {
-      console.log('Something went wront on request')
-      console.log(newInvoiceResponse);
-      return new Response(JSON.stringify({ code: 420, message: "Something went wrong on request" }));
-    }
-  }
-
-  return new Response(JSON.stringify({ code: 420, message: "Wrong payload" }));
 }
 
-export { handler as POST } 
+export { handler as POST }
